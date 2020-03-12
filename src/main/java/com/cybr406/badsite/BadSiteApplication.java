e7e08e092ce2ad926b55084730ab43c54046e128
Ry Lowry <rylowry@gmail.com>
2020-03-12 00:37:59 -0500

    First commit.

---
+++ src/main/java/com/cybr406/badsite/BadSiteApplication.java
@@ -0,0 +1,13 @@
+package com.cybr406.badsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cybr406/badsite/BadSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybr406.badsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
